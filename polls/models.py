767507932e5ccc5109ccb42c1916fad8c50bd02e
Vatsal Kanakiya <vatsal.kanakiya@somaiya.edu>
2016-12-10 04:08:20 +0000

    Initializing repo. Tutorial completed until part 2

---
+++ polls/models.py
@@ -0,0 +1,26 @@
+#Python imports
+import datetime
+
+#Django imports
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('date published')
+    #__str__ gives a string representation to the object
+    def __str__(self):
+        return self.question_text
+    #Function to tell if function is recent
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
+    #__str__ gives a string representation to the object
+    def __str__(self):
+        return self.choice_text
